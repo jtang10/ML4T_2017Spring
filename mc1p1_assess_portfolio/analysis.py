cae726ff72843775495cc4914c4434328d0d1624
Jingning Tang <tangjingning@gmail.com>
2017-02-27 20:49:08 -0600

    1st project finished

--- mc1p1_assess_portfolio/analysis.py
+++ mc1p1_assess_portfolio/analysis.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
 from util import get_data, plot_data
+import math
 
 # This is the function that will be tested by the autograder
 # The student must update this code to properly implement the functionality
@@ -20,20 +21,59 @@ def assess_portfolio(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
     prices = prices_all[syms]  # only portfolio symbols
     prices_SPY = prices_all['SPY']  # only SPY, for comparison later
 
+    alloc_asset = []
+    for i in range(len(syms)):
+        # Calculate cr for each stock
+        num_stock = sv * allocs[i] / prices[syms[i]][0]
+        alloc_asset.append(num_stock)
+
+    # Calculate cumulative return
+    def calculate_cr(prices, alloc_asset):
+        returns = prices.copy()
+        returns *= alloc_asset
+        returns_total = returns.sum(axis=1)
+        ev = returns_total[-1]
+        cr = ev / sv - 1
+        return cr
+
+    def calculate_adr_sddr(prices, alloc_asset):
+        daily_returns = prices.copy()
+        daily_returns *= alloc_asset
+        daily_returns_total = daily_returns.sum(axis=1)
+        # print daily_returns_total
+        dr = (daily_returns_total / daily_returns_total.shift(1)) - 1
+        dr[0] = 0
+        adr = dr[1:].mean()
+        sddr = dr[1:].std()
+        return adr, sddr
+
+    def calculate_sr(prices, alloc_asset, rfr, sf):
+        adr, sddr = calculate_adr_sddr(prices, alloc_asset)
+        daily_rf = math.pow(1 + rfr, 1/sf) - 1
+        sr = (adr - daily_rf) / sddr * math.sqrt(sf)
+        return sr
+
     # Get daily portfolio value
-    port_val = prices_SPY # add code here to compute daily portfolio values
+    port_val_all = prices.copy() # add code here to compute daily portfolio values
+    port_val_all *= alloc_asset
+    port_val = port_val_all.sum(axis=1)
+    port_val /= port_val[0]
+    prices_SPY /= prices_SPY[0]
 
     # Get portfolio statistics (note: std_daily_ret = volatility)
-    cr, adr, sddr, sr = [0.25, 0.001, 0.0005, 2.1] # add code here to compute stats
+    cr = calculate_cr(prices, alloc_asset)
+    adr, sddr = calculate_adr_sddr(prices, alloc_asset)
+    sr = calculate_sr(prices, alloc_asset, rfr, sf)
 
     # Compare daily portfolio value with SPY using a normalized plot
     if gen_plot:
         # add code to plot here
         df_temp = pd.concat([port_val, prices_SPY], keys=['Portfolio', 'SPY'], axis=1)
+        plot_data(df_temp)
         pass
 
     # Add code here to properly compute end value
-    ev = sv
+    ev = port_val[-1]
 
     return cr, adr, sddr, sr, ev
 
@@ -44,11 +84,11 @@ def test_code():
     # Define input parameters
     # Note that ALL of these values will be set to different values by
     # the autograder!
-    start_date = dt.datetime(2009,1,1)
-    end_date = dt.datetime(2010,1,1)
-    symbols = ['GOOG', 'AAPL', 'GLD', 'XOM']
-    allocations = [0.2, 0.3, 0.4, 0.1]
-    start_val = 1000000  
+    start_date = dt.datetime(2010,1,1)
+    end_date = dt.datetime(2010,12,31)
+    symbols = ['AXP', 'HPQ', 'IBM', 'HNZ']
+    allocations = [0.0, 0.0, 0.0, 1.0]
+    start_val = 1000000
     risk_free_rate = 0.0
     sample_freq = 252
 
@@ -57,7 +97,7 @@ def test_code():
         syms = symbols, \
         allocs = allocations,\
         sv = start_val, \
-        gen_plot = False)
+        gen_plot = True)
 
     # Print statistics
     print "Start Date:", start_date
--- mc1p1_assess_portfolio/test.py
+++ mc1p1_assess_portfolio/test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
 from util import get_data, plot_data
+import math
 
 # This is the function that will be tested by the autograder
 # The student must update this code to properly implement the functionality
@@ -29,44 +30,49 @@ prices_SPY = prices_all['SPY']  # only SPY, for comparison later
 # print prices['AAPL'][-1]
 
 alloc_asset = []
-daily_returns = prices.copy()
 for i in range(len(syms)):
     # Calculate cr for each stock
     num_stock = sv * allocs[i] / prices[syms[i]][0]
     alloc_asset.append(num_stock)
 
-ev_total = prices.ix[-1, :] * alloc_asset
-ev = ev_total.sum(axis=0)
-cr = ev/sv - 1
+# Calculate cumulative return
+def calculate_cr(prices, alloc_asset):
+    returns = prices.copy()
+    returns *= alloc_asset
+    returns_total = returns.sum(axis=1)
+    ev = returns_total[-1]
+    cr = ev / sv - 1
+    return cr
 
+def calculate_adr_sddr(prices, alloc_asset):
+    daily_returns = prices.copy()
+    daily_returns *= alloc_asset
+    daily_returns_total = daily_returns.sum(axis=1)
+    # print daily_returns_total
+    dr = (daily_returns_total / daily_returns_total.shift(1)) - 1
+    dr[0] = 0
+    adr = dr[1:].mean()
+    sddr = dr[1:].std()
+    return adr, sddr
 
-# Calculate daily return
-#print daily_returns[1:]
-returns = prices.copy()
-returns *= alloc_asset
-returns_total = returns.sum(axis=1)
-#print returns_total
-adr = (returns_total / returns_total.shift(1)) - 1
-adr[0] = 0
-print adr.mean()
-daily_returns = prices - prices.shift(1)
-daily_returns.ix[0, :] = 0
-# print daily_returns * alloc_asset
-daily_returns *= alloc_asset
-daily_returns_total = daily_returns.sum(axis=1)
+def calculate_sr(prices, alloc_asset, rfr, sf):
+    adr, sddr = calculate_adr_sddr(prices, alloc_asset)
+    daily_rf = math.pow(1 + rfr, 1/sf) - 1
+    sr = (adr - daily_rf) / sddr * math.sqrt(sf)
+    return sr
 
-
-print alloc_asset
-print ev
-print('cumulative return is %f' % (ev / sv -1))
-#print daily_returns
-
-
+cr = calculate_cr(prices, alloc_asset)
+adr, sddr = calculate_adr_sddr(prices, alloc_asset)
+sr = calculate_sr(prices, alloc_asset, rfr, sf)
+print cr, adr, sddr, sr
 # Get daily portfolio value
-port_val = prices_SPY # add code here to compute daily portfolio values
-
-# Get portfolio statistics (note: std_daily_ret = volatility)
-cr, adr, sddr, sr = [0.25, 0.001, 0.0005, 2.1] # add code here to compute stats
+port_val_all = prices.copy() # add code here to compute daily portfolio values
+port_val_all *= alloc_asset
+port_val = port_val_all.sum(axis=1)
+port_val /= port_val[0]
+prices_SPY /= prices_SPY[0]
+df_temp = pd.concat([port_val, prices_SPY], keys=['Portfolio', 'SPY'], axis=1)
+print df_temp
 
 # Compare daily portfolio value with SPY using a normalized plot
 if gen_plot:
